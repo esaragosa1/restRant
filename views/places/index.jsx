7c04f47e200c7dcc3468087ea3f2f17663e13aa0
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-11-04 22:18:43 -0700

    Adding places index page

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('../default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX</h1>
+          </main>
+      </Def>
+    )
+  }
+  
+
+module.exports = index
