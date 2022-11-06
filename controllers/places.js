5d8559f107cc99694e9104bf519e7476a5aa7feb
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-11-06 10:58:26 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -40,4 +40,19 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) { // !SWITCH (places [6]=false); so the switch makes it true
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] }) //function show (data) (don't want to write this each time=>data.place.name)
+  }
+})
+
+
+
 module.exports = router
