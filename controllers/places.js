360934edc65e3066c89a26b4a217da0993e88097
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-11-05 21:54:17 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,22 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 //POST
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 // GET /places
 router.get('/', (req, res) => {
@@ -13,7 +25,7 @@ router.get('/', (req, res) => {
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/smallPizza.jpg'
+    pic: '/images/smallPizza.jpeg'
   }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
