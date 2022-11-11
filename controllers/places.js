babc3f144c2b3e339d7b20d78f2c7e23ed56646a
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-11-11 10:36:24 -0800

    Implemented the Mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -1,100 +1,65 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
-//POST
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-
-// GET /places
 router.get('/', (req, res) => {
-  res.render('places/index', { places })
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', { places })
+  })
+  .catch(err => {
+    console.log(err) 
+    res.render('error404')
+  })
 })
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) { // !SWITCH (places [6]=false); so the switch makes it true
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id }) //function show (data) (don't want to write this each time=>data.place.name)
-  }
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
-//Edit Route
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id], id })
-  }
-})
 
-// PUT; here we are putting our user's edits back into our places array.
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
-// Delete Route
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
+
+
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,6 +7,7 @@ function new_form () {
           <main>
             <h1>Add a New Place</h1>
             <form method="POST" action="/places">
+
                 <div className="form-group">
                     <label htmlFor="name">Place Name</label>
                     <input className="form-control" id="name" name="name" required />
@@ -27,7 +28,14 @@ function new_form () {
                     <label htmlFor="cuisines">Cuisines</label>
                     <input className="form-control" id="cuisines" name="cuisines" required />
                 </div>
+
+                <div className="form-group">
+                    <label for="founded">Founded Year</label>
+                    <input className="form-control" id="founded" name="founded" />
+                </div>
+
                 <input className="btn btn-primary" type="submit" value="Add Place" />
+
                 </form>
           </main>
         </Def>
